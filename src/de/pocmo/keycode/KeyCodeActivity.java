11b93149681e4012c1f447ded23c262bbb12f9c9
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-06-26 20:24:08 +0200

    Initial - Base Project

---
+++ src/de/pocmo/keycode/KeyCodeActivity.java
@@ -0,0 +1,13 @@
+package de.pocmo.keycode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KeyCodeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
