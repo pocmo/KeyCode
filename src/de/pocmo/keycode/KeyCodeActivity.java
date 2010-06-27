563186ce47866f7fc35c3c659dc3666092e04db2
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-06-27 13:16:37 +0200

    Show log and key action

--- src/de/pocmo/keycode/KeyCodeActivity.java
+++ src/de/pocmo/keycode/KeyCodeActivity.java
@@ -22,10 +22,26 @@ public class KeyCodeActivity extends Activity {
         
         EditText input = (EditText) findViewById(R.id.input);
         final TextView view = (TextView) findViewById(R.id.keycode);
+        final EditText log  = (EditText) findViewById(R.id.log);
         
         input.setOnKeyListener(new OnKeyListener() {
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				String action = "?";
+				
+				switch (event.getAction()) {
+					case KeyEvent.ACTION_DOWN:
+						action = "DOWN";
+						break;
+					case KeyEvent.ACTION_UP:
+						action = "UP";
+						break;
+					case KeyEvent.ACTION_MULTIPLE:
+						action = "MULTIPLE";
+						break;
+				}
+				
 				view.setText(String.valueOf(keyCode));
+				log.setText(keyCode + " (" + action + ")\n" + log.getText());
 				return true;
 			}
         });
